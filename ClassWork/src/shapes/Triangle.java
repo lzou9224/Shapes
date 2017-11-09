c8ffac4cc5d9f17fc2c7a0ad04c90066946a3214
BT_1N3_34 <BT_1N3_34@BT_1N3_34.bktech.local>
2017-11-09 09:26:52 -0500

    upload

--- ClassWork/src/shapes/Triangle.java
+++ ClassWork/src/shapes/Triangle.java
@@ -1,5 +1,36 @@
 package shapes;
 
-public class Triangle {
+public class Triangle implements Shape
+{
+	private int base;
+	private int height;
+	private int side2;
+	private int side3;
+
+	public Triangle(int base, int height) 
+	{
+		super(base, height);
+		this.base = base;
+		this.height = height;
+	}
+
+	public double calculateArea() 
+	{
+		return (1/2*this.base*this.height);
+	}
+
+
+	public double calculatePerimeter() {
+		return (this.base+this.side2+this.side3);
+	}
+	
+	@Override
+	public String toString() 
+	{ 
+		return "Triangle base: " + base + "Triangle height: " + height + "Triangle side2" + side2 + "Triangle side3:" "Area: " + this.calculateArea() + " Perimeter: "
+				+ this.calculatePerimeter(); 
+	}
+
+}
 
 }
